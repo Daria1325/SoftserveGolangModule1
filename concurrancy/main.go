28b6de1fbc7a6ad1246301508cb57157ab1cb4a8
Daria Bozhenko <ravena1893@gmail.com>
2021-11-20 17:47:08 +0200

    done

--- concurrancy/main.go
+++ concurrancy/main.go
@@ -1,45 +1,3 @@
-//package main
-//
-//import (
-//	"fmt"
-//	"sync"
-//	"time"
-//)
-//
-//type Message struct {
-//	time time.Time
-//	msg string
-//}
-//func print(indiv Message){
-//	fmt.Println(indiv.time.Format("15:04:05"),indiv.msg)
-//}
-//
-//
-//func main(){
-//	var wg sync.WaitGroup
-//	wg.Add(2)
-//	intCh := make(chan Message)
-//	send := func(intCh chan Message) {
-//		defer wg.Done()
-//		for{
-//			intCh <- Message{time.Now(),"time"}
-//			time.Sleep(2 * time.Second)
-//		}
-//	}
-//	recv := func(intCh chan Message) {
-//		defer wg.Done()
-//		for{
-//			i:=<-intCh
-//			print(i)
-//			time.Sleep(5 * time.Second)
-//		}
-//	}
-//
-//		go send(intCh)
-//		go recv(intCh)
-//		wg.Wait()
-//}
-//TODO context
 package main
 
 import (
@@ -60,42 +18,38 @@ func print(indiv Message) {
 
 func main() {
 	var wg sync.WaitGroup
-	wg.Add(3)
+
 	intCh := make(chan Message)
 	send := func(intCh chan Message) {
 		defer wg.Done()
-		for {
-			intCh <- Message{time.Now(), "time"}
-			time.Sleep(2 * time.Second)
-		}
+		intCh <- Message{time.Now(), "time"}
+		time.Sleep(2 * time.Second)
 	}
 	recv := func(intCh chan Message) {
 		defer wg.Done()
-		for {
-			i := <-intCh
-			print(i)
-			time.Sleep(5 * time.Second)
-		}
+		i := <-intCh
+		print(i)
+		time.Sleep(5 * time.Second)
 	}
 	group := func() {
 		defer wg.Done()
 		go send(intCh)
 		go recv(intCh)
-		wg.Wait()
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	_ = ctx
 	defer cancel()
 
-	select {
-	case <-time.After(time.Microsecond):
-		go group()
-		wg.Wait()
-	case <-ctx.Done():
-		fmt.Println(ctx.Err())
-		//return
-
-		//wg.Wait()
-
+	for {
+		select {
+		case <-time.After(time.Microsecond):
+			wg.Add(3)
+			go group()
+			wg.Wait()
+		case <-ctx.Done():
+			fmt.Println(ctx.Err())
+			return
+		}
 	}
 }
